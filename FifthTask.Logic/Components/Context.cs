212a68c919c4e4219e318ffd5fee4837f9e992d8
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-15 21:44:04 +0100

    FifTask.Logic was changed. Interface IExpression and Class Context were added

---
+++ FifthTask.Logic/Components/Context.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace FifthTask.Logic.Components
+{
+    public class Context
+    {
+        public Dictionary<string, string> DictionaryNumbers { get; }
+        public string[] Numbers { get; }
+
+        public Context(params string[] numbers)
+        {
+            Numbers = numbers;
+
+            DictionaryNumbers = new()
+            {
+                { "0", "zero" },
+                { "1", "one" },
+                { "2", "two" },
+                { "3", "three" },
+                { "4", "four" },
+                { "5", "five" },
+                { "6", "six" },
+                { "7", "seven" },
+                { "8", "eight" },
+                { "9", "nine" }
+            };
+        }
+    }
+}
---
+++ FifthTask.Logic/Components/Expressions/Interfaces/IExpression.cs
@@ -0,0 +1,7 @@
+﻿namespace FifthTask.Logic.Components.Expressions.Interfaces
+{
+    public interface IExpression
+    {
+        string Interpret(Context context);
+    }
+}
