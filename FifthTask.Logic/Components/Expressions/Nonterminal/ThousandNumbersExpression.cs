7399ce249cd8241394a6d37b31cffaba013fcbfc
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-15 23:05:16 +0100

    FifTask.Logic was changed. Class ThousandNumbersExpression were added

---
+++ FifthTask.Logic/Components/Expressions/Nonterminal/ThousandNumbersExpression.cs
@@ -0,0 +1,26 @@
+﻿using FifthTask.Logic.Components.Expressions.Helpers;
+using FifthTask.Logic.Components.Expressions.Interfaces;
+
+namespace FifthTask.Logic.Components.Expressions.Nonterminal
+{
+    public class ThousandNumbersExpression : IExpression
+    {
+        private const string NUMBER_CLASS = "thousand";
+
+        private IExpression _number;
+        private IExpression _hundredNumberExpression;
+
+        public ThousandNumbersExpression(IExpression number, IExpression hundredNumberExpression) 
+        {
+            _number = number;
+            _hundredNumberExpression = hundredNumberExpression;
+        }
+
+        public string Interpret(Context numberContext) => _number.Interpret(numberContext) switch
+        {
+            "0" or "00" or "000" => _hundredNumberExpression.Interpret(numberContext),
+            _ => $"{NonterminalExpressionHelper.GetHundredInerpret(_number.Interpret(numberContext))} {NUMBER_CLASS} " +
+                    $"{_hundredNumberExpression.Interpret(numberContext)}"
+        };
+    }
+}
