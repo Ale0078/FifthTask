c4c30123c1ced5e98d5ac77552c0101a9f4b4054
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-15 21:57:23 +0100

    FifTask.Logic was changed. Class OneNumbersExpression were added

---
+++ FifthTask.Logic/Components/Expressions/Nonterminal/OneNumbersExpression.cs
@@ -0,0 +1,19 @@
+﻿using FifthTask.Logic.Components.Expressions.Interfaces;
+
+namespace FifthTask.Logic.Components.Expressions.Nonterminal
+{
+    internal class OneNumbersExpression : IExpression
+    {
+        private readonly IExpression _number;
+
+        public OneNumbersExpression(IExpression number) 
+        {
+            _number = number;
+        }
+
+        public string Interpret(Context context) 
+        {
+            return context.DictionaryNumbers[_number.Interpret(context)];
+        }
+    }
+}
