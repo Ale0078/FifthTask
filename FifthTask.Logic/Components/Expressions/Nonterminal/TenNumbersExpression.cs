410e2363720f0c7b9cdd05ad722f9fdedb128129
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-15 22:16:31 +0100

    FifTask.Logic was changed. Class TenNumbersExpression were added

---
+++ FifthTask.Logic/Components/Expressions/Nonterminal/TenNumbersExpression.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+using FifthTask.Logic.Components.Expressions.Interfaces;
+
+namespace FifthTask.Logic.Components.Expressions.Nonterminal
+{
+    internal class TenNumbersExpression : IExpression
+    {
+        private readonly Dictionary<string, string> _tenNumbers = new()
+        {
+            { "1", "ten" },
+            { "2", "twenty" },
+            { "3", "thirthty" },
+            { "4", "forty" },
+            { "5", "fifty" },
+            { "6", "sixty" },
+            { "7", "seventy" },
+            { "8", "eighty" },
+            { "9", "ninety" }
+        };
+
+        private readonly Dictionary<string, string> _alternativeTenNumbers = new()
+        {
+            { "1", "eleven" },
+            { "2", "twelve" },
+            { "3", "thirteen" }
+        };
+
+        private readonly IExpression _number;
+        private readonly IExpression _oneNumberExpression;
+
+        public TenNumbersExpression(IExpression number, IExpression oneNumberExpression) 
+        {
+            _number = number;
+            _oneNumberExpression = oneNumberExpression;
+        }
+
+        public string Interpret(Context numberContext) => _number.Interpret(numberContext) switch 
+        {
+            "0" => _oneNumberExpression.Interpret(numberContext),
+            "1" => (_oneNumberExpression.Interpret(numberContext)) switch
+            {
+                "zero" => _tenNumbers["1"],
+                "one" => _alternativeTenNumbers["1"],
+                "two" => _alternativeTenNumbers["2"],
+                "three" => _alternativeTenNumbers["3"],
+                "eight" => _oneNumberExpression.Interpret(numberContext) + "een",
+                _ => _oneNumberExpression.Interpret(numberContext) + "teen",
+            },
+            _ => _tenNumbers[_number.Interpret(numberContext)] + 
+                    (_oneNumberExpression.Interpret(numberContext) == "zero" ? "" : " " + _oneNumberExpression.Interpret(numberContext)),
+        };
+    }    
+}
