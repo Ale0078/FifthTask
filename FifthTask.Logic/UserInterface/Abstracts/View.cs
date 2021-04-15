d1df5c2584ff29a4931b64e39c876a34e8458729
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-15 23:34:21 +0100

    FifTask.Logic was changed. Abstract Class View were added

---
+++ FifthTask.Logic/UserInterface/Abstracts/View.cs
@@ -0,0 +1,14 @@
+﻿namespace FifthTask.Logic.UserInterface.Abstracts
+{
+    public abstract class View
+    {
+        public Model ViewModel { get; }
+
+        public View(Model viewMolde) 
+        {
+            ViewModel = viewMolde;
+        }
+
+        public abstract void Display(string number);
+    }
+}
