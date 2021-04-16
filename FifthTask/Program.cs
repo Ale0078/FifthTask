5cb1884b7b56e8abac6690dbac1a69c8be502c50
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-16 02:26:21 +0100

    FifTask was changed. Class Startup was added

--- FifthTask/Program.cs
+++ FifthTask/Program.cs
@@ -1,29 +1,9 @@
-﻿using System;
+﻿using static System.Console;
 
-using FifthTask.Controllers;
-using FifthTask.Views;
-using FifthTask.Models;
-using FifthTask.Logic.Components;
-using FifthTask.Logic.Components.Expressions.Interfaces;
-using FifthTask.Logic.Components.Expressions.Nonterminal;
-using FifthTask.Logic.Components.Expressions.Terminal;
+using FifthTask;
 
-Context context = new Context("990", "919", "4", "0", "9");
+Startup projectStart = new();
 
-IExpression number1 = new NumberExpression(0);
-IExpression number2 = new NumberExpression(1);
-IExpression number3 = new NumberExpression(2);
-IExpression number4 = new NumberExpression(3);
-IExpression number5 = new NumberExpression(4);
+projectStart.Start(args);
 
-IExpression expression = new MillionNumbersExpression(number1,
-    thousandNumbersExpression: new ThousandNumbersExpression(number2,
-    hundredNumberExpression: new HundredNumbersExpression(number3,
-    tenNumberExpression: new TenNumbersExpression(number4,
-    oneNumberExpression: new OneNumbersExpression(number5)))));
-
-Console.WriteLine(expression.Interpret(context));
-
-NumberController n = new NumberController(new NumberView(new NumberViewModel()));
-n.SetModel("9990");
-n.Display();
+ReadKey();
---
+++ FifthTask/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using NLog;
+
+using static System.Console;
+
+using FifthTask.Controllers;
+using FifthTask.Models;
+using FifthTask.Views;
+using FifthTask.Logic.UserInterface.Abstracts;
+using LibToTasks.Builders;
+using LibToTasks.Validation.Interfaces;
+
+namespace FifthTask
+{
+    public class Startup
+    {
+        private const int MAX_RANGE = 999999999;
+        private const int MIN_RANGE = 000000000;
+
+        private readonly ILogger _logger;
+        private readonly IValidator _validator;
+        private readonly ITransformator _transformator;
+
+        public Startup() 
+        {
+            _logger = LogManager.GetCurrentClassLogger();
+
+            _validator = new DefaultValidatorBuilder().Create();
+            _transformator = new DefaultTransformatorBuilder().Create();
+        }
+
+        public void Start(string[] args) 
+        {
+            if (args.Length != 1) 
+            {
+                GetMessage();
+
+                return;
+            }
+
+            if (!uint.TryParse(args[0], out _ )) 
+            {
+                GetMessage();
+
+                return;
+            }
+
+            if (!CheckingValue(args[0])) 
+            {
+                GetMessage();
+
+                return;
+            }
+
+            Controller numberController = new NumberController(
+                numberView: new NumberView(
+                    viewModel: new NumberViewModel()));
+
+            numberController.SetModel(args[0]);
+
+            numberController.Display();
+
+            _logger.Info("Program is finalized");
+        }
+
+        private bool CheckingValue(string valueToCheck) 
+        {
+            return _validator.CheckValue(checkingValue =>
+            {
+                if (checkingValue > MAX_RANGE || checkingValue < MIN_RANGE)
+                {
+                    return false;
+                }
+
+                return true;
+            }, _transformator.ConfirmConversion<int, string>(valueToCheck), false);
+        }
+
+        private void GetMessage() 
+        {
+            WriteLine("You must enter only one number from 999999999 to 000000000");
+
+            _logger.Error("You must enter only one number number from 999999999 to 000000000");
+            _logger.Info("Program is finalized");
+        }
+    }
+}
